519daad3b9b6943b5c57e810a4bd1dbe3b7b95b1
[ravencovington] <rcov00@gmail.com>
2015-03-16 14:02:18 -0400

    Added images to seed file

---
+++ spec/controllers/sessions_controller_spec.rb
@@ -0,0 +1,41 @@
+require 'spec_helper'
+
+describe SessionsController, type: :controller do
+  describe "GET new" do
+    it "renders :new" do
+      get :new
+      expect(response).to puts 'In SessionsController new'
+    end
+  end
+
+  describe "POST create" do
+    before do 
+      User.destroy_all
+      @user = FactoryGirl.create(:user)
+    end
+    
+    context "valid info" do
+      it "signs in user" do
+        post :create, session: { email: @user.email, password: @user.password }
+        controller.should be_signed_in
+    end
+
+      it "redirects to users#show" do
+        post :create, session: { email: @user.email, password: @user.password }
+        expect(response).to redirect_to(user_path(@user.id))
+      end
+    end
+
+    context "invalid info" do
+      it "does not sign in user" do
+      post :create, session: { email: @user.email, password: "wrong password" }
+        controller.should_not be_signed_in
+      end
+
+      it "re-renders :new" do
+        post :create, session: { email: @user.email, password: "wrong password"}
+        expect(response).to render_template(:new)
+      end
+    end
+  end
+end
---
+++ spec/models/users_model_spec.rb
@@ -0,0 +1,138 @@
+require 'spec_helper'
+
+describe User, type: :model do
+  before { @user = User.new(name: "Foo Bar", email: "foo@foobar.com",
+    password: "foobar", password_confirmation: "foobar") }
+ 
+  subject { @user }
+
+  it { should respond_to(:name) }
+  it { should respond_to(:email) }
+  it { should respond_to(:password_digest) }
+  it { should respond_to(:password) }
+  it { should respond_to(:password_confirmation) }
+  it { should respond_to(:authenticate) }
+  it { should respond_to(:remember_token) }
+
+  it { should be_valid }
+
+  describe "validations" do
+    describe "name" do
+      context "not present" do 
+        before { @user.name = "" }
+        it { should_not be_valid }
+      end
+    
+      context "too long" do
+        before { @user.name = "a" * 51 }
+        it { should_not be_valid }
+      end
+    end
+
+    describe "email" do
+      context "not present" do 
+        before { @user.email = " " }
+        it { should_not be_valid }
+      end
+
+      context "too long" do
+        before { @user.email = "a" * 101 }
+        it { should_not be_valid }
+      end
+
+      context "correct format" do
+        it "is valid" do
+          addresses = %w[user@foo.COM A_US-ER@f.b.org frst.lst@foo.jp a+b@baz.cn]
+          addresses.each do |valid_address|
+            @user.email = valid_address
+            expect(@user).to be_valid
+          end
+        end
+      end 
+
+      context "incorrect format" do
+        it "is invalid" do  
+          addresses = %w[user@foo,com user_at_foo.org example.user@foo.
+                       foo@bar_baz.com foo@bar+baz.com foo@bar..com]
+          addresses.each do |invalid_address|
+            @user.email = invalid_address
+            expect(@user).to_not be_valid
+          end
+        end
+      end
+
+      context "mixed case" do
+        it "is saved as lowercase" do
+          mixed_case_email = "fooBAR@exaMPLE.coM"
+          @user.email = mixed_case_email
+          @user.save
+          expect(@user.reload.email).to eq mixed_case_email.downcase
+        end
+      end
+
+      context "when already taken" do
+        before do
+          user_with_same_email = @user.dup
+          user_with_same_email.save
+        end
+
+        it { should_not be_valid }
+
+        User.destroy_all
+      end
+    end
+  
+    describe "password" do
+      context "not present" do
+        before { @user.password = @user.password_confirmation = " " }
+        it { should_not be_valid }
+      end
+
+      context "too short" do  
+        before { @user.password = @user.password_confirmation = "a" * 5 }
+        it { should_not be_valid }
+      end
+
+      context "too long" do
+        before { @user.password = @user.password_confirmation = "a" * 51 }
+        it { should_not be_valid }
+      end
+
+      context "doesn't match confirmation" do
+        before { @user.password_confirmation = "mismatch" }
+        it { should_not be_valid }
+      end
+
+      describe "return value of authenticate method" do
+        before do
+          User.destroy_all
+          @user.save
+        end
+        let(:found_user) { User.find_by(email: @user.email) }
+
+          context "with valid password" do
+            it { should eq found_user.authenticate(@user.password) }
+          end
+
+          context "with invalid password" do
+            let(:user_for_invalid_password) { found_user.authenticate("invalid-password") }
+
+            it { should_not eq user_for_invalid_password }
+            specify { expect(user_for_invalid_password).to be_falsey }
+        end
+      end
+    end
+
+    describe "remember_token" do
+      before do
+        User.destroy_all
+        @user.save
+      end
+         
+
+      it "has a default value" do
+        expect(@user.remember_token).to_not be_blank
+      end
+    end
+  end
+end
